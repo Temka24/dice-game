a4fac63210dd66d931af3d187999875fcc1e02ac
Temuujin <thxdeadshotxht@gmail.com>
2023-11-13 00:42:42 +0700

    Togloomo sevev

--- index.js
+++ index.js
@@ -1,4 +1,90 @@
-var dicenumber = Math.floor(Math.random()+1)
-document.getElementById("btn-roll").addEventListener(onclick, function(){
-    document.querySelector(".dice").src = "dice" + dicenumber + ".png"
+
+var activePlayer = 1
+
+var currentscore = 0
+
+ var relativeScore1 = 0
+ var relativeScore2 = 0
+
+document.getElementById("btn-roll").addEventListener("click", function(){
+
+    var dicenumber = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("dice").src = "dice" + dicenumber + ".png"
+
+    if(dicenumber !== 1){
+        currentscore = currentscore + dicenumber
+        document.getElementById("current-" + activePlayer).textContent = currentscore
+    }
+    else{
+        currentscore = 0
+
+        document.getElementById("current-" + activePlayer).textContent = 0
+
+        if(activePlayer === 1){
+            activePlayer = 2
+        }
+        else{
+            activePlayer = 1
+        }
+    }
+
 } )
+
+document.getElementById("btn-hold").addEventListener("click", function(){
+
+    if(activePlayer === 1){
+
+      relativeScore1 = relativeScore1 + currentscore
+    
+     document.getElementById("score-1").textContent = relativeScore1
+      
+     if(relativeScore1 >= 50){
+        alert("Player 1 win ")
+     }
+
+     activePlayer = 2
+
+     currentscore = 0
+
+     document.getElementById("current-1").textContent = currentscore
+
+    }
+    else{ 
+
+        relativeScore2 = relativeScore2 + currentscore
+        
+        document.getElementById("score-2").textContent = relativeScore2
+
+        if(relativeScore2 >= 50){
+        alert("Player 2 win")
+    }
+
+        activePlayer = 1
+
+        currentscore = 0
+
+        document.getElementById("current-2").textContent = currentscore
+
+    }
+
+})
+
+
+
+document.getElementById("btn-new").addEventListener("click", function(){
+
+    document.getElementById("score-1").textContent = currentscore
+
+    document.getElementById("score-2").textContent = currentscore
+
+    currentscore = 0
+    
+    activePlayer = 1
+     
+    relativeScore1 = 0
+
+    relativeScore2 = 0
+
+})
+
